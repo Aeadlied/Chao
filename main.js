758a882125c9d96447cb9708b51a605d96f66eb7
Aeadlied <85672696+Aeadlied@users.noreply.github.com>
2023-06-30 16:17:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,9 +11,18 @@ async function readLineFromFile(filePath) {
     // 获取指定行的内容并返回
    const lineContent = lines[lineNumber - 1];
     // 使用正则表达式匹配行号和句子
-    const matchResult = lineContent.match(/^(\d+)\.(.*)$/);
+    const matchResult = lineContent.match(/^(\d+)\.\s+(.*)$/);
     // 如果匹配成功，则返回去掉行号的句子；否则返回原始内容
-    return matchResult ? matchResult[2].trim() : lineContent.trim();
+    const content = matchResult ? matchResult[2].trim() : lineContent.trim();
+    // 计算在一起的天数
+    const now = new Date();
+    const start = new Date('2023-02-04');
+    const diffInDays = Math.floor((now - start) / (1000 * 60 * 60 * 24));
+    // 计算认识的天数
+    const start2 = new Date('2005-09-01');
+    const diffInDays2 = Math.floor((now - start2) / (1000 * 60 * 60 * 24));
+    // 拼接返回值
+    return `今天是我们在一起的第${diffInDays}天\n今天是我们认识的第${diffInDays2}天\n今日份问候：${content}`;
   } catch (error) {
     console.error(error);
   }
