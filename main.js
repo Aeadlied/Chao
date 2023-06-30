3d61d9687f508ba9428f631689dc57ba9b54c145
Aeadlied <85672696+Aeadlied@users.noreply.github.com>
2023-06-30 23:30:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ const notify1 = async (contents) => {
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({
       token,
-      title: '来自超超的每日请安1',
+      title: '来自超超的每日请安',
       content: contents,
       template: 'markdown',
     }),
@@ -60,7 +60,7 @@ const notify2 = async (contents) => {
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({
       token,
-      title: '来自超超的每日请安2',
+      title: '来自超超的每日请安',
       content: contents,
       template: 'markdown',
     }),
